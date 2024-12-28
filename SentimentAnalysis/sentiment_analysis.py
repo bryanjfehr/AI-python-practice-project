4de71f8976435c33550ebacbb12c73d8cf0a0082
bryanjfehr <bryanjfehr@gmail.com>
2024-12-27 23:33:12 -0500

    Edited folder structure, added __init__.py fle, formatting in the sentiment_analysis.py file.

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -5,4 +5,7 @@ def sentiment_analyzer(text_to_analyse):  # Define a function named sentiment_an
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
+    formatted_response = json.loads(response)
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
+    return {'label': label, 'score': score}
