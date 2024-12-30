06df99aee5b3cdebfcf8dee3ccf1acf34067876d
bryanjfehr <bryanjfehr@gmail.com>
2024-12-29 20:46:51 -0500

    Completed the practice project with error checking on the iput field and static code analysis on the server.py file.

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,12 +1,26 @@
 import requests
 import json  # Import the requests library to handle HTTP requests
 
-def sentiment_analyzer(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+def sentiment_analyzer(text_to_analyze):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+    """Sends text to the watson library for sentiment analysis, then formats the response as JSON and returns the score and label as a dictionary pair"""
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL of the sentiment analysis service
-    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
+    
+    myobj = { "raw_document": { "text": text_to_analyze } }  # Create a dictionary with the text to be analyzed
+    
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
+    
     response = requests.post(url, json=myobj, headers=header)  # Send a POST request to the API with the text and headers
+    
     formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
+    
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    
+    #If the response code is 500, set label and score to "None"
+    elif response.status_code == 500:
+        label = None
+        score = None
+
+    #Return the label and score as a dictionary
     return {'label': label, 'score': score}
--- server.py
+++ server.py
@@ -4,28 +4,36 @@
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
-import flask from Flask
-from flask import requests, render_template
-from sentiment_analysis import sentiment_analyzer
-#Initiate the flask app : TODO
+from flask import Flask
+from flask import request, render_template
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-@app.route("/sentimentAnalyzer", methods="POST")
-def sent_analyzer(text_for_analysis):
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
-    '''
-    # TODO
+#Initiate the flask app
+app = Flask("Sentiment Analyzer")
+
+@app.route("/sentimentAnalyzer")
+def sent_analyzer():
+    """Sends text from the HTML form for analysis using the method in sentiment_analysis.py"""
+    #Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    #Pass the text to the analyzer function
+    response = sentiment_analyzer(text_to_analyze)
+
+    #Extract the label and score from the response
+    label = response['label']
+    score = response['score']
+
+    #Check if label is None, indicating err or invalid response
+    if label is None:
+        return "Invalid input ! Try again."
+    #Return a formatted string with the outputs
+    return f"The given text has been identified as {label} with a score of {score}."
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
-    #TODO
+    """Renders the webpage for entering Strings and viewing results."""
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
-
+    app.run(host="0.0.0.0", port=5000)
